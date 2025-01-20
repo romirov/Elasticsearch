7602330bacd20519d14a67bfc89f480a27c335d6
maru <romirov@gmail.com>
2025-01-21 00:28:41 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.marulab.elastic"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.opensearch:opensearch-testcontainers:2.1.2")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/marulab/elk/TestcontainersConfiguration.kt
@@ -0,0 +1,23 @@
+package com.marulab.elk
+
+import org.opensearch.testcontainers.OpensearchContainer
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.utility.DockerImageName
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+	@Bean
+	fun elasticsearchContainer(): OpensearchContainer<*> = container
+
+	private companion object {
+		val OPENSEARCH_IMAGE = DockerImageName.parse("opensearchproject/opensearch:2.11.0")
+		@Container
+		val container = OpensearchContainer(OPENSEARCH_IMAGE)
+			.withEnv("discovery.type", "single-node")
+			.withEnv("OPENSEARCH_JAVA_OPTS", "-Xms512m -Xmx512m")
+			.withExposedPorts(9200)
+	}
+}
